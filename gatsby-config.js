6914d6a2e6f5c5e0ec9e8396a94743006b0fd110
660693468 <xirek@iMac-xirek.local>
2021-03-05 14:18:52 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -84,7 +84,7 @@ module.exports = {
         short_name: `Foundation`,
         start_url: `/`,
         background_color: `#f7f0eb`,
-        theme_color: `#a2466c`,
+        theme_color: `#cc3700`,
         display: `standalone`,
         icon: "static" + settings.meta.iconimage,
       },
