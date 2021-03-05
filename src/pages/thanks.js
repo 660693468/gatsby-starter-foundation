4c3b02a88ca34af42ca3438127db14472c3750b0
660693468 <xirek@iMac-xirek.local>
2021-03-05 13:26:07 +0100

    Update thanks.js

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -15,9 +15,10 @@ const Thanks = () => (
         fontSize: "128px",
         color: "var(--primary-color)"
       }}/>
-      <h1>Got your message</h1>
-      <p>Thank you for getting in touch us. We will get back to you shortly.</p>
-      <Link to="/" className="button"><RiArrowLeftSLine className="icon -left"/>Lets go back to Homepage</Link>
+      <h1>Wysłana</h1>
+      <p>Dziękuję za wiadomość.</p>
+      <Link to="/" className="button"><RiArrowLeftSLine className="icon -left"/>Strona główna
+</Link>
     </div>
 
   </Layout>
