6b8223faa3693781c177010af3c8a7e5a9a625c7
660693468 <xirek@iMac-xirek.local>
2021-03-05 14:34:10 +0100

    Update index.js

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -2,7 +2,7 @@ export default {
   colors: {
     text: '#000',
     background: '#fff',
-    primary: '#5C2941',
+    primary: '#e63e00',
     accent: '#fff',
     muted: 'rgba(0, 0, 0, 0.7)',
     cardBg: '#fff',
