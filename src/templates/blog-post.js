fb0f32ba8e14469ee556d3feee8e9c91bc8286f7
660693468 <xirek@iMac-xirek.local>
2021-03-07 06:07:38 +0100

    Update blog-post.js

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -63,7 +63,7 @@ const Post = ({ data, pageContext }) => {
   const { markdownRemark } = data // data.markdownRemark holds your post data
   const { frontmatter, html, excerpt } = markdownRemark
   const Image = frontmatter.featuredImage ? frontmatter.featuredImage.childImageSharp.fluid : ""
-  const { poprzedni, następny } = pageContext
+  const { previous, next } = pageContext
 
   let props = {
     previous,
